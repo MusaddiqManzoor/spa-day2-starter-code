e3df5b682c92bbd06f8cdb23f98264a8a27e3eba
Musaddiqmanzoor <musaddiqmanzoor4@gmail.com>
2021-09-03 12:59:26 -0500

    Studio-with-some-todos-and-bonus-yet-to-add

---
+++ spa-day-starter-code-user-signup-starter/src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,31 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        if (!(user.getPassword().isBlank()) && !(verify.isBlank()) && verify.equals(user.getPassword())) {
+            model.addAttribute("userName", user.getUserName());
+            return "user/index";
+        }
+        model.addAttribute("errors", "Password doesn't match!");
+        return "redirect:/user/add";
+    }
+
+}
